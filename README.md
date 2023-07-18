484c3d13bf3c00249e6eeb5f0a94acc2c30d70d0
Bob Plotts <bob.plotts@dremio.com>
2023-07-18 18:11:36 -0400

    Fix Slack updates.

--- README.md
+++ README.md
@@ -2,4 +2,27 @@
 
 Delete AWS "available" volumes. These are orphaned when an EC2 instance stops and the associated EBS volume was not set to "delete on terminate."
 
+This program can send Slack messages - it depends on another small library - `github.com/rcprcp/SimpleSlack` - feel free to remove that code if it does not work for your use case.
 
+Currently tested with Java17. 
+
+This program uses an environment variable containing the names of the people that will recceive the Slack messages: 
+
+```export SLACK_NOTIFICATION_LIST="bobp;Mickey;Minnie123```
+
+## Download 
+```shell
+git clone https://github.com/rcprcp/DeleteVolumes
+```
+### Build
+cd into the DeleteVolumes directory then: 
+
+```shell
+mvn clean package 
+```
+
+### Run 
+
+```shell
+java -jar target/DeleteVolumes-1.0-SNAPSHOT-jar-with-dependencies.jar 
+```
--- src/main/java/com/cottagecoders/DeleteVolumes.java
+++ src/main/java/com/cottagecoders/DeleteVolumes.java
@@ -2,6 +2,10 @@ package com.cottagecoders;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
+import com.cottagecoders.simpleslack.FetchMembers;
+import com.cottagecoders.simpleslack.SendSlackMessage;
+import com.cottagecoders.simpleslack.userlist.Member;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
@@ -17,6 +21,10 @@ import software.amazon.awssdk.services.ec2.model.Instance;
 import software.amazon.awssdk.services.ec2.model.Reservation;
 import software.amazon.awssdk.services.ec2.model.Volume;
 
+import java.io.IOException;
+import java.util.Date;
+import java.util.Map;
+
 
 public class DeleteVolumes {
   private static final Logger LOG = LogManager.getLogger(DeleteVolumes.class);
@@ -24,24 +32,20 @@ public class DeleteVolumes {
   @Parameter(names = "--dryRun", description = "Dry run - don't make changes; don't delete anything")
   private static boolean dryRun = false;
 
-  @Parameter(names = {"--help"}, description = "Help")
-  private static boolean help = false;
-
   public static void main(String[] args) {
     DeleteVolumes dv = new DeleteVolumes();
     JCommander jc = JCommander.newBuilder().addObject(dv).build();
     jc.parse(args);
-
-    if (help) {
-      jc.usage();
-      return;
-    }
 
     dv.iterateEc2Instances();
 
   }
 
   private void iterateEc2Instances() {
+    StringBuilder sb = new StringBuilder(2000);
+    String msg = String.format("DeleteVolumes: %s\n", new Date());
+    sb.append(msg);
+
     int gb = 0;
     try (Ec2Client client = Ec2Client.builder().credentialsProvider(ProfileCredentialsProvider.create()).region(Region.US_EAST_2).build()) {
 
@@ -52,7 +56,8 @@ public class DeleteVolumes {
           DescribeInstancesRequest request = DescribeInstancesRequest.builder().build();
           DescribeInstancesResponse instancesResponse = ec2.describeInstances(request);
 
-          LOG.info("Region: {}", region.regionName());
+          LOG.info(String.format("Region: %s", region.regionName()));
+
           for (Reservation reservation : instancesResponse.reservations()) {
             for (Instance instance : reservation.instances()) {
               DescribeVolumesResponse resp;
@@ -75,7 +80,63 @@ public class DeleteVolumes {
         }
       }
     }
-    LOG.info("{} gb. approximate cost {} ", gb, gb * 0.08d);
 
+    // make this message look nice.
+    if (gb == 0) {
+      msg = "0 gb of detached volumes!  :smile:";
+    } else {
+      msg = String.format("%d gb. of detached volumes! approximate savings  :dollar-spin: %.2f  \n", gb, gb * 0.08d);
+    }
+
+    LOG.info(msg);
+    sb.append(msg);
+    slackIt(sb.toString());
+
+  }
+
+  void slackIt(String msg) {
+
+    // fetch and parse the notification list.
+    String envVar = System.getenv("SLACK_NOTIFICATION_LIST");
+    if (StringUtils.isEmpty(envVar)) {
+      System.out.println("No notifications - SLACK_NOTIFICATION_LIST is empty.");
+      return;
+    }
+
+    String[] slackDisplayNames = envVar.split(",");
+    if (slackDisplayNames.length == 0) {
+      System.out.println("No notifications - can't split SLACK_NOTIFICATION_LIST.");
+      return;
+    }
+
+    try {
+      FetchMembers fetch = new FetchMembers();
+
+      Map<String, Member> members = null;
+      // gather list of members from Slack
+      members = fetch.fetchAllMembers();
+
+
+      for (String s : slackDisplayNames) {
+        // get specific user
+        Member member = members.get(s.trim());
+        if (member == null) {
+          // user lookup failed.
+          continue;
+        }
+        // TODO: delete debugging code.
+        if (member.getName().contains("bobp") || member.getName().contains("lark")) {
+          System.out.println("stop. name matched");
+        }
+
+        SendSlackMessage ssm = new SendSlackMessage();
+        ssm.sendDM(member.getId(), msg);
+      }
+
+    } catch (IOException | InterruptedException ex) {
+      System.out.println("fetch.fetchAllMembers() exception: " + ex.getMessage());
+      ex.printStackTrace();
+      System.exit(12);
+    }
   }
 }
